32fc55ae424ae9393c59a717eda14e61ebee3cc5
Mohammad Anwari <mdamt@ridon.id>
2017-10-22 22:23:35 +0700

    Expose signedDate

--- lib/src/main/java/app/sivionmobile/ridon/id/lib/PdfVerification.java
+++ lib/src/main/java/app/sivionmobile/ridon/id/lib/PdfVerification.java
@@ -9,18 +9,21 @@ public class PdfVerification {
   final private String location;
   final private String reason;
   final private Date date;
+  final private Date signedDate;
 
   public PdfVerification(
       List<SignatureVerification> sigs,
       final String name,
       final String location,
       final String reason,
-      final Date date) {
+      final Date date,
+      final Date signedDate) {
     this.sigs = sigs;
     this.name = name;
     this.location = location;
     this.reason = reason;
     this.date = date;
+    this.signedDate = signedDate;
   }
 
   public String name() {
@@ -38,6 +41,8 @@ public class PdfVerification {
   public Date date() {
     return this.date;
   }
+
+  public Date signedDate() { return this.signedDate; }
 
   public List<SignatureVerification> signatures() {
     return this.sigs;
